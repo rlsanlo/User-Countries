6f9a4c5da34dc1e399c129141dcd188644bf9703
Ronaldo Lopes <rlsanlo@gmail.com>
2020-05-29 22:00:21 -0300

    listar usuarios conforme seu país de origem

--- JS/script.js
+++ JS/script.js
@@ -1,45 +1,101 @@
-let users = [];
-let countries = [];
-let usersAndCountries = [];
+let globalUsers = [];
+let globalCountries = [];
+let globalUsersAndCountries = [];
 
 async function start(){
-    console.log('Dom carregado');
+    const p1 = promiseUsers();
+    const p2 = promiseCountries();
 
-    await fetchUsers();
-    await fetchCountries();
+    await Promise.all([p1, p2]);
+
     hideSpinner();
     mergeUsersAndCountries();
     render();
 }
 
+function promiseUsers(){
+    return new Promise(async(resolve, reject) => {
+        const users = await fetchUsers();
+        setTimeout( () => {
+            resolve(users);
+        }, 5000);
+    });
+}
+
+
+function promiseCountries(){
+    return new Promise(async(resolve, reject) => {
+        const countries = await fetchCountries();
+        setTimeout( () => {
+            resolve(countries);
+        }, 6000);
+    });
+}
+
 async function fetchUsers(){
     const res = await fetch('https://randomuser.me/api/?seed=javascript&results=100&nat=BR&noinfo');
     const json = await res.json();
-    users = json.results.map(({ name, picture, nat }) =>{
+    globalUsers = json.results.map(({ name, picture, nat }) =>{
         return {
-            Username: name.first,
-            Userpicture: picture.large,
-            Usercountry: nat
+            userName: name.first,
+            userPicture: picture.large,
+            userCountry: nat
+        }
+    });
+}
+
+async function fetchCountries(){
+    const res = await fetch('https://restcountries.eu/rest/v2/all');
+    const json = await res.json();
+    globalCountries = json.map(({name, alpha2Code, flag}) =>{
+        return {
+            countryName: name,
+            countryCode: alpha2Code,
+            countryFlag: flag
         }
     });
     
-
 }
 
-function fetchCountries(){
-    const res = await fetch('https://restcountries.eu/rest/v2/all');
-    const json = await res.json();
-    countries = json.map((countries) =>{
-        return {
-            countryNme: name.first,
-            countryCode: picture.large,
-            countryFlag: nat,
-        }
+function hideSpinner(){
+    const spinner = document.querySelector('#spinner');
+    spinner.classList.add('hide');
+}
+
+function mergeUsersAndCountries(){
+    globalUsersAndCountries = [];
+
+    globalUsers.forEach((user) => {
+        const userCountry = globalCountries.find((country) => {
+            return country.countryCode === user.userCountry;
+        });
+
+        globalUsersAndCountries.push({...user, ...userCountry});
     });
+
+    console.log(globalUsersAndCountries);
 }
 
-function hideSpinner(){}
-function mergeUsersAndCountries(){}
-function render(){}
+function render(){
+    const divUsers = document.querySelector('#divUsers');
+
+    divUsers.innerHTML = `
+        <div class='row'>
+            ${globalUsersAndCountries.map(({userPicture, userName, countryFlag}) => {
+                return `
+                    <div class='col s6 m4 l3'>
+                        <div class= 'flex-row bordered' >
+                            <img class='avatar' src='${userPicture}' />
+                            <div class='flex-column' >
+                            <span>${userName}</span>
+                            <img class='flag' src='${countryFlag}' />
+                            </div>
+                        </div>
+                    </div>
+                `;
+            }).join(' ')}
+        </div>
+    `;
+}
 
 start();
--- README.md
+++ README.md
@@ -1,2 +1,14 @@
 # User-Countries
- Listar usuarios por país de origem
+---
+Projeto simples, feito em JavaScript puro, tem como objetivo consumir duas apis externas, uma de usuarios outra de países, e listar na tela os usuarios juntamente com bandeira do seu país de origem.
+---
+# Linguagem utilizada
+---
+* HTML
+* CSS
+* Java Script
+* Framework Materialize
+---
+# Demonstração
+---
+![User-Countries](./User-Countries.gif)
